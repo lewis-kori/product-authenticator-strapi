56a69686460689322153a833da2f899563940312
lewis-kori <lewiskosh@gmail.com>
2020-12-18 12:09:50 +0300

    setup content types

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7ad09091e08850d1c451d10ee5ef657'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05d05724-8c5c-4ef7-9a2d-112398d82f59'
+};
